071b6f67fe24067d71cfefee4e330425aab44b01
Danield05 <joseaquino971@gmail.com>
2025-11-15 18:48:40 -0600

    Hacer fuente negra en la vista de detalles de planilla

--- resources/views/vistas/planillas/show.blade.php
+++ resources/views/vistas/planillas/show.blade.php
@@ -21,63 +21,63 @@ Detalles de Planilla de Sueldo
                             <div class="row mb-4">
                                 <div class="col-md-6">
                                     <h5 class="text-primary">Información del Empleado</h5>
-                                    <table class="table table-borderless">
+                                    <table class="table table-borderless" style="color: black;">
                                         <tr>
                                             <th width="150">Nombre:</th>
-                                            <td><strong style="color: black;">{{ $planilla->empleado->nombre }} {{ $planilla->empleado->apellido }}</strong></td>
+                                            <td>{{ $planilla->empleado->nombre }} {{ $planilla->empleado->apellido }}</td>
                                         </tr>
                                         <tr>
                                             <th>Período:</th>
-                                            <td><strong style="color: black;">{{ $planilla->periodo }}</strong></td>
+                                            <td>{{ $planilla->periodo }}</td>
                                         </tr>
                                         <tr>
                                             <th>Fecha Inicio:</th>
-                                            <td><strong style="color: black;">{{ $planilla->fecha_inicio ? \Carbon\Carbon::parse($planilla->fecha_inicio)->format('d/m/Y') : 'N/A' }}</strong></td>
+                                            <td>{{ $planilla->fecha_inicio ? \Carbon\Carbon::parse($planilla->fecha_inicio)->format('d/m/Y') : 'N/A' }}</td>
                                         </tr>
                                         <tr>
                                             <th>Fecha Fin:</th>
-                                            <td><strong style="color: black;">{{ $planilla->fecha_fin ? \Carbon\Carbon::parse($planilla->fecha_fin)->format('d/m/Y') : 'N/A' }}</strong></td>
+                                            <td>{{ $planilla->fecha_fin ? \Carbon\Carbon::parse($planilla->fecha_fin)->format('d/m/Y') : 'N/A' }}</td>
                                         </tr>
                                         <tr>
                                             <th>Días Laborados:</th>
-                                            <td><strong style="color: black;">{{ $planilla->dias_laborados }}</strong></td>
+                                            <td>{{ $planilla->dias_laborados }}</td>
                                         </tr>
                                         <tr>
                                             <th>Días con Permiso:</th>
-                                            <td><strong style="color: black;">{{ $planilla->dias_faltados_con_permiso }}</strong></td>
+                                            <td>{{ $planilla->dias_faltados_con_permiso }}</td>
                                         </tr>
                                         <tr>
                                             <th>Días sin Permiso:</th>
-                                            <td><strong style="color: black;">{{ $planilla->dias_faltados_sin_permiso }}</strong></td>
+                                            <td>{{ $planilla->dias_faltados_sin_permiso }}</td>
                                         </tr>
                                     </table>
                                 </div>
                                 <div class="col-md-6">
                                     <h5 class="text-success">Cálculos Salariales</h5>
-                                    <table class="table table-borderless">
+                                    <table class="table table-borderless" style="color: black;">
                                         <tr>
                                             <th width="150">Salario Base:</th>
-                                            <td><strong style="color: black;">${{ number_format($planilla->salario_base, 2) }}</strong></td>
+                                            <td>${{ number_format($planilla->salario_base, 2) }}</td>
                                         </tr>
                                         <tr>
                                             <th>AFP (7.25%):</th>
-                                            <td><strong style="color: black;">${{ number_format($planilla->afp_empleado, 2) }}</strong></td>
+                                            <td>${{ number_format($planilla->afp_empleado, 2) }}</td>
                                         </tr>
                                         <tr>
                                             <th>ISSS (3%):</th>
-                                            <td><strong style="color: black;">${{ number_format($planilla->iss_empleado, 2) }}</strong></td>
+                                            <td>${{ number_format($planilla->iss_empleado, 2) }}</td>
                                         </tr>
                                         <tr>
                                             <th>Renta:</th>
-                                            <td><strong style="color: black;">${{ number_format($planilla->renta, 2) }}</strong></td>
+                                            <td>${{ number_format($planilla->renta, 2) }}</td>
                                         </tr>
                                         <tr>
                                             <th>Total Deducciones:</th>
-                                            <td><strong style="color: black;">${{ number_format($planilla->total_deducciones, 2) }}</strong></td>
+                                            <td>${{ number_format($planilla->total_deducciones, 2) }}</td>
                                         </tr>
                                         <tr>
                                             <th>Sueldo Neto:</th>
-                                            <td><strong class="text-success" style="font-size: 1.2em;">${{ number_format($planilla->sueldo_neto, 2) }}</strong></td>
+                                            <td class="text-success" style="font-size: 1.2em;">${{ number_format($planilla->sueldo_neto, 2) }}</td>
                                         </tr>
                                     </table>
                                 </div>
@@ -86,14 +86,14 @@ Detalles de Planilla de Sueldo
                             <div class="row">
                                 <div class="col-md-6">
                                     <h5 class="text-info">Beneficios</h5>
-                                    <table class="table table-borderless">
+                                    <table class="table table-borderless" style="color: black;">
                                         <tr>
                                             <th width="150">Aguinaldo:</th>
-                                            <td><strong style="color: black;">${{ number_format($planilla->aguinaldo, 2) }}</strong></td>
+                                            <td>${{ number_format($planilla->aguinaldo, 2) }}</td>
                                         </tr>
                                         <tr>
                                             <th>Vacaciones:</th>
-                                            <td><strong style="color: black;">${{ number_format($planilla->vacaciones, 2) }}</strong></td>
+                                            <td>${{ number_format($planilla->vacaciones, 2) }}</td>
                                         </tr>
                                     </table>
                                 </div>
