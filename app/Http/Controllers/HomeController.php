f8bff0e2bbbf55514c33f05ad88ef48722a13c4b
Danield05 <joseaquino971@gmail.com>
2025-11-16 01:29:13 -0600

    Add logging to HomeController index for debugging 500 error with contador user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,39 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        Log::info('Accediendo a home index', ['user_id' => auth()->id()]);
+
+        try {
+            $user = auth()->user();
+            Log::info('Usuario: ' . $user->name);
+
+            // Test counts
+            $empresaCount = \App\Models\empresa::count();
+            Log::info('Empresa count: ' . $empresaCount);
+
+            $periodoCount = \App\Models\Periodo::count();
+            Log::info('Periodo count: ' . $periodoCount);
+
+            $userCount = \App\Models\User::count();
+            Log::info('User count: ' . $userCount);
+
+            $balanceCount = \App\Models\BalanceGeneral::count();
+            Log::info('Balance count: ' . $balanceCount);
+
+            $estadoCount = \App\Models\estado_resultado::count();
+            Log::info('Estado count: ' . $estadoCount);
+
+            // Test roles
+            $hasContador = $user->hasRole('Contador');
+            Log::info('Has role Contador: ' . ($hasContador ? 'yes' : 'no'));
+
+            $roles = $user->roles;
+            Log::info('Roles count: ' . $roles->count());
+
+            return view('home');
+        } catch (\Exception $e) {
+            Log::error('Error en home index: ' . $e->getMessage(), ['trace' => $e->getTraceAsString()]);
+            throw $e;
+        }
     }
 }
